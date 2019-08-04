0956527428c48f1e1d036b18aca78415481cc59c
Alexey Ulashchick <a.ulashchick@gmail.com>
2019-08-03 19:04:24 -0700

    Lint fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
 import { HomePage } from './pages/home/Home';
-import { ImagesList } from './components/ImagesList/ImagesList';
-import { List } from 'immutable';
-import { ImageModel } from './models/ImageModel';
 import { style } from 'typestyle';
 
 const greeting: string = 'alexeyphoto.zone';
 
-const testData: List<ImageModel> = List([
-  new ImageModel('1', 'https://alexeyphoto.zone/assets/images/1-20180905-122358/768.jpg'),
-  new ImageModel('2', 'https://alexeyphoto.zone/assets/images/2-20180907-041126/768.jpg'),
-  new ImageModel('2', 'https://alexeyphoto.zone/assets/images/3-20180524-192817/768.jpg')
-]);
+// const testData: List<ImageModel> = List([
+//   new ImageModel('1', 'https://alexeyphoto.zone/assets/images/1-20180905-122358/768.jpg'),
+//   new ImageModel('2', 'https://alexeyphoto.zone/assets/images/2-20180907-041126/768.jpg'),
+//   new ImageModel('2', 'https://alexeyphoto.zone/assets/images/3-20180524-192817/768.jpg')
+// ]);
 
 const app = style({
   height: '100%'
