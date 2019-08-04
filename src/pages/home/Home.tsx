7a4be0c326dfdee4cd36993d90c71b579e8bb56b
Alexey Ulashchick <a.ulashchick@gmail.com>
2019-08-04 10:24:10 -0700

    + Extract styles into separate typescript file

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,47 +1,7 @@
 import React, { useState, SetStateAction, Dispatch } from 'react';
 import { Header } from '../../components/Header/Header';
-import { style, media } from 'typestyle';
 import { Nav } from '../../components/Nav/Nav';
-
-const homePage = style({
-  backgroundImage: `url('/assets/images/20180525-192644.jpg')`,
-  height: '100vh',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: '50% 50%'
-});
-
-const header = style(
-  {
-    position: 'fixed',
-    bottom: '5em',
-    left: 0,
-    right: 0
-  },
-  media(
-    { maxWidth: '600px' },
-    {
-      top: '4em',
-      bottom: 'inherit'
-    }
-  )
-);
-
-const nav = style(
-  {
-    position: 'fixed',
-    top: '5em',
-    left: 0,
-    right: 0
-  },
-  media(
-    { maxWidth: '600px' },
-    {
-      bottom: '5em',
-      top: 'inherit'
-    }
-  )
-);
+import { HomePageClass, HeaderClass, NavClass } from './HomeStyles';
 
 interface IProps {
   title: string;
@@ -51,9 +11,9 @@ export const HomePage: React.FC<IProps> = (props: IProps) => {
   const [counter, setNumber]: [number, (Dispatch<SetStateAction<number>>)] = useState<number>(0);
 
   return (
-    <div className={homePage} onClick={() => setNumber(counter + 1)}>
-      <Header className={header} title={'Travelling & Photography'} subTitle={'alexeyphoto.zone'} />
-      <Nav className={nav} />
+    <div className={HomePageClass} onClick={() => setNumber(counter + 1)}>
+      <Header className={HeaderClass} title={'Travelling & Photography'} subTitle={'alexeyphoto.zone'} />
+      <Nav className={NavClass} />
     </div>
   );
 };
---
+++ src/pages/home/HomeStyles.tsx
@@ -0,0 +1,41 @@
+import { style, media } from 'typestyle';
+
+export const HomePageClass = style({
+  backgroundImage: `url('/assets/images/20180525-192644.jpg')`,
+  height: '100vh',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: '50% 50%'
+});
+
+export const HeaderClass = style(
+  {
+    position: 'fixed',
+    bottom: '5em',
+    left: 0,
+    right: 0
+  },
+  media(
+    { maxWidth: '600px' },
+    {
+      top: '4em',
+      bottom: 'inherit'
+    }
+  )
+);
+
+export const NavClass = style(
+  {
+    position: 'fixed',
+    top: '5em',
+    left: 0,
+    right: 0
+  },
+  media(
+    { maxWidth: '600px' },
+    {
+      bottom: '5em',
+      top: 'inherit'
+    }
+  )
+);
