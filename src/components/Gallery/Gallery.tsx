2c575de9a332cdde671a7856be7891dc31ff3f88
Alexey Ulashchick <a.ulashchick@gmail.com>
2019-08-06 21:23:06 -0700

    Galerry Scroll V2

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Range } from 'immutable';
-import { ImageStyle, GalleryScrollableContainerStyle, DebuggingDiv, GalleryContainer } from './GalleryStyle';
+import { ImageStyle, GalleryScrollableContainerStyle, DebuggingDiv, GalleryContainer, FakeImageStyle, topLine } from './GalleryStyle';
 import { useEffect } from 'react';
 import { number } from 'prop-types';
 
@@ -18,11 +18,18 @@ export const Gallery = React.memo(
     const sticked = new Set<number>();
     const IMG_HEIGTH = 250;
     const IMG_GAP = 500;
+    const BLOCK = IMG_GAP + IMG_HEIGTH;
+    const PILE_ZONE = 0.15; //%
 
     useEffect(() => {
       const debuggerDiv: HTMLElement = document.getElementById('debugger')!;
       const scrollableContainer: HTMLElement = document.getElementById(`${hash}_scrollableContainer`)!;
       const HEIGHT = scrollableContainer.clientHeight;
+      const SCROLL_AREA_START = PILE_ZONE * HEIGHT;
+      const SCROLL_AREA = HEIGHT - 2 * SCROLL_AREA_START;
+
+      console.log(`Initial scroll=${scrollableContainer.scrollTop}`);
+      console.log(`H=${HEIGHT}, START=${SCROLL_AREA_START}, AREA=${SCROLL_AREA}`);
 
       list.forEach((_, index) => {
         hashMap.set(index, document.getElementById(`${hash}_${index}`)!);
@@ -33,71 +40,76 @@ export const Gallery = React.memo(
           const start = scrollableContainer.scrollTop;
           const end = start + HEIGHT;
 
-          const firstBlock = 1 + Math.floor((start - IMG_HEIGTH) / (IMG_HEIGTH + IMG_GAP));
-          const lastBlock = Math.floor(end / (IMG_HEIGTH + IMG_GAP));
+          const firstPotentialBlock = 1 + Math.floor((start - BLOCK) / BLOCK);
+          const firstBlock = start + SCROLL_AREA_START < BLOCK + firstPotentialBlock * BLOCK ? firstPotentialBlock : firstPotentialBlock + 1;
 
-          debuggerDiv.innerHTML = '';
+          const lastPotentialBlock = Math.floor((end - IMG_GAP) / BLOCK);
+          const lastBlock = end - SCROLL_AREA_START > IMG_GAP + lastPotentialBlock * BLOCK ? lastPotentialBlock : lastPotentialBlock - 1;
 
-          for (let index = firstBlock; index <= lastBlock; index++) {
-            const deg = list.get(index)!;
-            const imgCenter = IMG_HEIGTH / 2 + index * (IMG_HEIGTH + IMG_GAP);
+          debuggerDiv.innerHTML = `sroll=${end - SCROLL_AREA_START}, [${firstBlock},${lastBlock}]`;
 
-            //Max distanse from center of the viewport
-            const max = HEIGHT / 2 + IMG_HEIGTH / 2;
-            const distance = start + HEIGHT / 2 - imgCenter;
-            const k = distance / max;
+          // debuggerDiv.innerHTML = `sroll=${start + SCROLL_AREA_START}, [${firstBlock},${lastBlock}]`;
 
-            debuggerDiv.innerHTML += `<div>[${index}]=${list.get(index)}, 1-k=${1 - Math.abs(k)}</div>`;
+          // for (let index = firstBlock; index <= lastBlock; index++) {
+          // const deg = list.get(index)!;
+          // const imgCenter = IMG_HEIGTH / 2 + index * (IMG_HEIGTH + IMG_GAP);
 
-            if (1 - Math.abs(k) > 0.3) {
-              if (sticked.has(index)) {
-                sticked.delete(index);
-                hashMap.get(index)!.style.visibility = 'inherit';
-                if (clonesMap.has(index)) {
-                  clonesMap.get(index)!.parentNode!.removeChild(clonesMap.get(index)!);
-                }
-              }
-              // hashMap.get(index)!.style.transform = `rotate(${k * deg}deg) scale(${0.5 + 0.5 * (1 - Math.abs(k))})`;
-              // hashMap.get(index)!.style.transform = `rotate(${k * deg}deg)`;
-            } else if (!sticked.has(index)) {
-              console.log('here');
-              sticked.add(index);
-              const clone: HTMLElement = hashMap.get(index)!.cloneNode(true) as HTMLElement;
-              const rect = hashMap.get(index)!.getBoundingClientRect();
-              console.log(rect);
+          // //Max distanse from center of the viewport
+          // const max = HEIGHT / 2 + IMG_HEIGTH / 2;
+          // const distance = start + HEIGHT / 2 - imgCenter;
+          // const k = distance / max;
 
-              clone.setAttribute('id', `${clone}_${clone.getAttribute('id')}`);
-              clone.style.transform = hashMap.get(index)!.style.transform;
-              clone.style.position = 'absolute';
-              clone.style.top = `${rect.top}px`;
-              clone.style.left = `${rect.left}px`;
-              clone.style.transition = '0.2s all ease';
-              clone.style.zIndex = '0';
-              scrollableContainer.appendChild(clone);
-              clonesMap.set(index, clone);
-              requestAnimationFrame(() => {
-                if (k > 0) {
-                  clone.style.top = '-150px';
-                } else {
-                }
-                // clone.style.top = '0';
-                // clone.style.left = '0';
-              });
+          // debuggerDiv.innerHTML += `<div>[${index}]=${list.get(index)}, 1-k=${1 - Math.abs(k)}</div>`;
 
-              hashMap.get(index)!.style.visibility = 'hidden';
-            }
+          // if (1 - Math.abs(k) > 0.3) {
+          //   if (sticked.has(index)) {
+          //     sticked.delete(index);
+          //     hashMap.get(index)!.style.visibility = 'inherit';
+          //     if (clonesMap.has(index)) {
+          //       clonesMap.get(index)!.parentNode!.removeChild(clonesMap.get(index)!);
+          //     }
+          //   }
+          //   // hashMap.get(index)!.style.transform = `rotate(${k * deg}deg) scale(${0.5 + 0.5 * (1 - Math.abs(k))})`;
+          //   // hashMap.get(index)!.style.transform = `rotate(${k * deg}deg)`;
+          // } else if (!sticked.has(index)) {
+          //   console.log('here');
+          //   sticked.add(index);
+          //   const clone: HTMLElement = hashMap.get(index)!.cloneNode(true) as HTMLElement;
+          //   const rect = hashMap.get(index)!.getBoundingClientRect();
+          //   console.log(rect);
 
-            // if (1 - Math.abs(k) < 0.3 && !sticked.has(index)) {
-            //   hashMap.get(index)!.style.visibility = 'hidden';
-            //   sticked.add(index);
-            //   const clone: HTMLElement = hashMap.get(index)!.cloneNode() as HTMLElement;
-            //   console.log(clone);
-            // } else {
+          //   clone.setAttribute('id', `${clone}_${clone.getAttribute('id')}`);
+          //   clone.style.transform = hashMap.get(index)!.style.transform;
+          //   clone.style.position = 'absolute';
+          //   clone.style.top = `${rect.top}px`;
+          //   clone.style.left = `${rect.left}px`;
+          //   clone.style.transition = '0.2s all ease';
+          //   clone.style.zIndex = '0';
+          //   scrollableContainer.appendChild(clone);
+          //   clonesMap.set(index, clone);
+          //   requestAnimationFrame(() => {
+          //     if (k > 0) {
+          //       clone.style.top = '-150px';
+          //     } else {
+          //     }
+          //     // clone.style.top = '0';
+          //     // clone.style.left = '0';
+          //   });
 
-            //   hashMap.get(index)!.style.visibility = 'inherit';
-            //   hashMap.get(index)!.style.transform = `rotate(${k * deg}deg) scale(${0.5 + 0.5 * (1 - Math.abs(k))})`;
-            // }
-          }
+          //   hashMap.get(index)!.style.visibility = 'hidden';
+          // }
+
+          // if (1 - Math.abs(k) < 0.3 && !sticked.has(index)) {
+          //   hashMap.get(index)!.style.visibility = 'hidden';
+          //   sticked.add(index);
+          //   const clone: HTMLElement = hashMap.get(index)!.cloneNode() as HTMLElement;
+          //   console.log(clone);
+          // } else {
+
+          //   hashMap.get(index)!.style.visibility = 'inherit';
+          //   hashMap.get(index)!.style.transform = `rotate(${k * deg}deg) scale(${0.5 + 0.5 * (1 - Math.abs(k))})`;
+          // }
+          // }
         })
       );
     });
@@ -107,11 +119,12 @@ export const Gallery = React.memo(
         <div id={`${hash}_scrollableContainer`} className={GalleryScrollableContainerStyle}>
           <div className={DebuggingDiv} id="debugger" />
           {list.map((deg, index) => (
-            <div id={`${hash}_${index}`} className={ImageStyle} key={index} style={{ transform: `rotate(${0}deg)` }}>
+            <div id={`${hash}_${index}`} className={FakeImageStyle} key={index}>
               {index}
             </div>
           ))}
         </div>
+        <div className={topLine} />
       </div>
     );
   },
--- src/components/Gallery/GalleryStyle.ts
+++ src/components/Gallery/GalleryStyle.ts
@@ -5,37 +5,98 @@ import { style } from 'typestyle';
 export const GalleryContainer = style({
   position: 'relative',
   overflow: 'hidden',
+  $nest: {
+    '&::before': {
+      content: `''`,
+      position: 'absolute',
+      left: 0,
+      right: '50vw',
+      height: '1px',
+      backgroundColor: 'red',
+      top: '15%'
+    },
+    '&::after': {
+      content: `''`,
+      position: 'absolute',
+      left: 0,
+      right: '50vw',
+      height: '1px',
+      backgroundColor: 'red',
+      bottom: '15%'
+    }
+  }
 });
 
 export const GalleryScrollableContainerStyle = style({
   height: '80vh',
   width: '50vw',
   overflow: 'auto',
-  backgroundColor: GREY.toString(),
+  backgroundColor: GREY.toString()
 });
 
-export const ImageStyle = style({
-  width: '250px',
-  height: '250px',
-  backgroundColor: YELLOW.toString(),
-  margin: '0 auto 500px',
-  fontSize: '10em',
-  boxShadow: `3px 3px 10px ${BLACK.toString()}`,
-  position: 'relative',
-  zIndex: 1,
-  $nest: {
-    '&:last-of-type': {
-      marginBottom: '0'
+export const ImageStyle = style(
+  {
+    width: '250px',
+    height: '250px',
+    backgroundColor: YELLOW.toString(),
+    margin: '0 auto 500px',
+    fontSize: '10em',
+    boxShadow: `3px 3px 10px ${BLACK.toString()}`,
+    position: 'relative',
+    zIndex: 1,
+    $nest: {
+      '&:last-of-type': {
+        marginBottom: '0'
+      }
     }
   },
-}, flexWithCenteredContent);
+  flexWithCenteredContent
+);
 
-export const DebuggingDiv = style({
-  position: 'fixed',
-  top: 0,
-  right: 0,
-  width: '550px',
-  height: '50px',
-  backgroundColor: YELLOW.desaturate('50%').toString(),
-  color: BLACK.toString(),
-}, flexWithCenteredContent);
+export const FakeImageStyle = style(
+  {
+    width: '250px',
+    height: '250px',
+    backgroundColor: YELLOW.toString(),
+    opacity: 0.2,
+    margin: '500px auto',
+    fontSize: '10em',
+    position: 'relative',
+    $nest: {
+      '&:after': {
+        content: 'A',
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: '50%',
+        height: '1px',
+        backgroundColor: YELLOW.toString()
+      }
+    }
+  },
+  flexWithCenteredContent
+);
+
+export const topLine = style({
+  position: 'absolute',
+  left: 0,
+  right: '50vw',
+  top: '25%',
+  backgroundColor: 'green',
+  bottom: '25%',
+  pointerEvents: 'none',
+  opacity: 0.1,
+});
+
+export const DebuggingDiv = style(
+  {
+    position: 'fixed',
+    top: 0,
+    right: 0,
+    width: '550px',
+    height: '50px',
+    backgroundColor: YELLOW.desaturate('50%').toString(),
+    color: BLACK.toString()
+  },
+  flexWithCenteredContent
+);
