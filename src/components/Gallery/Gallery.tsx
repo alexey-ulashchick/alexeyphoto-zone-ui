488dbdc091df9c8c417a0e68cec962934cad54bf
Alexey Ulashchick <a.ulashchick@gmail.com>
2019-08-10 10:30:55 -0700

    Fefactoring

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,104 +1,48 @@
 import * as React from 'react';
 import { Range, List } from 'immutable';
-import { ImageStyle, GalleryScrollableContainerStyle, DebuggingDiv, GalleryContainer, IMG_HEIGTH, IMG_GAP, PILE_ZONE } from './GalleryStyle';
+import { ImageStyle, GalleryScrollableContainerStyle, GalleryContainer, IMG_HEIGTH, IMG_GAP, PILE_ZONE } from './GalleryStyle';
 import { useEffect } from 'react';
+import { getVisibilityCalcFn, VisibilityFn, RndFactor, PositionFn, getPositionCalcFn, ElStyle } from './GelleyHelpers';
 
 const DECK_SIZE = 20;
-
-type VisibilityFn = (scrollTop: number) => { start: number; end: number } | null;
-type PositionFn = (index: number, scrollTop: number) => { top: string; left: string; zIndex: number, transform: string };
-type RndFactor = { offsetX: number; offsetY: number; deg: number };
-
-function scaleFn(x: number): number {
-  return -0.5 * x * x *x*x+ 1;
-}
-
-function getVisibilityCalcFn(viewportHeight: number, visibilityPadding: number, blockHeight: number, gap: number): VisibilityFn {
-  const BLOCK = blockHeight + gap;
-
-  return (scrollTop: number): { start: number; end: number } | null => {
-    const end = scrollTop + viewportHeight;
-
-    const firstPotentialBlock = 1 + Math.floor((scrollTop - BLOCK) / BLOCK);
-    const firstBlock = scrollTop + visibilityPadding < BLOCK + firstPotentialBlock * BLOCK ? firstPotentialBlock : firstPotentialBlock + 1;
-
-    const lastPotentialBlock = Math.floor((end - gap) / BLOCK);
-    const lastBlock = end - visibilityPadding > gap + lastPotentialBlock * BLOCK ? lastPotentialBlock : lastPotentialBlock - 1;
-
-    return firstBlock > lastBlock ? null : { start: firstBlock, end: lastBlock };
-  };
-}
-
-function getPositionCalcFn(viewportHeight: number, visibilityPadding: number, imgHeight: number, gap: number, rndList: List<RndFactor>): PositionFn {
-  const BLOCK = imgHeight + gap;
-  const VISIBILITY_ZONE = viewportHeight - 2 * visibilityPadding;
-
-  function calcX(top: number, initialOffset: number, initialDeg: number): {offsetX: number, deg: number, scale: number} {
-    const kCenter = Math.abs((viewportHeight / 2) - (top + imgHeight /2)) / (VISIBILITY_ZONE /2 + imgHeight /2);
-    return {offsetX: kCenter * kCenter * initialOffset, deg: kCenter * kCenter * initialDeg, scale: scaleFn(kCenter)};
-  }
-
-  return (index: number, scrollTop: number): { top: string; left: string; zIndex: number, transform: string } => {
-    const rnd: RndFactor = rndList.get(index) || {offsetX: 0, offsetY: 0, deg: 0};
-    const topLine = scrollTop + visibilityPadding + rnd.offsetY;
-    const bottomLine = scrollTop + viewportHeight - visibilityPadding - rnd.offsetY;
-
-    let top: number, zIndex: number;
-
-    //If Block is higher than top bar
-    if (BLOCK * (index + 1) < topLine) {
-      top = visibilityPadding - imgHeight + rnd.offsetY;
-      zIndex = index;
-    } else if (BLOCK * (index + 1) - imgHeight > bottomLine) {
-      top = viewportHeight - visibilityPadding - rnd.offsetY;
-      zIndex = DECK_SIZE - 1 - index;
-    } else {
-      top =  BLOCK * (index + 1) - imgHeight - scrollTop;
-      zIndex = DECK_SIZE;
-    }
-
-    const {offsetX, deg, scale} = calcX(top, rnd.offsetX, rnd.deg);
-
-    return {
-      top: `${top}px`,
-      left: `calc(15vw + ${offsetX}px)`,
-      transform: `rotate(${deg}deg) scale(${scale}) translateZ(0)`,
-      zIndex
-    };
-  };
-}
+const ANGLE_DEVIATION = 10; // Angle randomization factor
+const POSITION_DEVIATION = 15; // Offset randomization factor
+const RND_LIST: List<RndFactor> = Range(0, DECK_SIZE)
+  .map(() => ({
+    offsetX: Math.round(Math.random() * 2 * POSITION_DEVIATION - POSITION_DEVIATION),
+    offsetY: Math.round(Math.random() * POSITION_DEVIATION),
+    deg: Math.round(Math.random() * ANGLE_DEVIATION * 2 - ANGLE_DEVIATION)
+  }))
+  .toList();
 
 export interface IGalleyProps {}
 
 export const Gallery = React.memo(
   (props: IGalleyProps) => {
-    const ANGLE_DEVIATION = 10; // Angle randomization factor
-    const POSITION_DEVIATION = 15; // Offset randomization factor
     const hash = Date.now();
-    const hashMap = new Map<number, HTMLElement>();
-    const RND_LIST: List<RndFactor> = Range(0, DECK_SIZE)
-      .map(() => ({
-        offsetX: Math.round(Math.random() * 2 * POSITION_DEVIATION - POSITION_DEVIATION),
-        offsetY: Math.round(Math.random() * POSITION_DEVIATION),
-        deg: Math.round(Math.random() * ANGLE_DEVIATION * 2 - ANGLE_DEVIATION)
-      }))
-      .toList();
+    const elMap = new Map<number, HTMLElement>();
+
+    function applyStyle(index: number, style: ElStyle): HTMLElement {
+      const el = elMap.get(index)!;
+      el.style.top = style.top;
+      el.style.left = style.left;
+      el.style.zIndex = String(style.zIndex);
+      el.style.transform = style.transform;
+
+      return el;
+    }
 
     useEffect(() => {
       const scrollableContainer: HTMLElement = document.getElementById(`${hash}_scrollableContainer`)!;
       const visibilityFn: VisibilityFn = getVisibilityCalcFn(scrollableContainer.clientHeight, PILE_ZONE, IMG_HEIGTH, IMG_GAP);
-      const positionFn: PositionFn = getPositionCalcFn(scrollableContainer.clientHeight, PILE_ZONE, IMG_HEIGTH, IMG_GAP, RND_LIST);
+      const positionFn: PositionFn = getPositionCalcFn(scrollableContainer.clientHeight, PILE_ZONE, IMG_HEIGTH, IMG_GAP, RND_LIST, DECK_SIZE);
 
       Range(0, DECK_SIZE).forEach(index => {
         const el: HTMLElement = document.getElementById(`${hash}_${index}`)!;
 
-        hashMap.set(index, el);
+        elMap.set(index, el);
 
-        const { top, left, zIndex, transform } = positionFn(index, scrollableContainer.scrollTop);
-        el.style.top = top;
-        el.style.left = left;
-        el.style.zIndex = String(zIndex);
-        el.style.transform = transform;
+        applyStyle(index, positionFn(index, scrollableContainer.scrollTop));
         el.style.visibility = 'inherit';
       });
 
@@ -120,12 +64,7 @@ export const Gallery = React.memo(
               : { start: Math.min(currentBlocks.start, newBlocks.start), end: Math.max(currentBlocks.end, newBlocks.end) };
 
           for (let index = start; index <= end; index++) {
-            const { top, left, zIndex, transform } = positionFn(index, scrollableContainer.scrollTop);
-            const el = hashMap.get(index)!;
-            el.style.top = top;
-            el.style.left = left;
-            el.style.transform = transform;
-            el.style.zIndex = String(zIndex);
+            applyStyle(index, positionFn(index, scrollableContainer.scrollTop));
           }
 
           currentBlocks = newBlocks;
@@ -136,7 +75,7 @@ export const Gallery = React.memo(
     return (
       <div id={`${hash}_container`} className={GalleryContainer}>
         <div id={`${hash}_scrollableContainer`} className={GalleryScrollableContainerStyle}>
-            <div style={{height: `${(IMG_HEIGTH + IMG_GAP) * DECK_SIZE + IMG_GAP}px`}}></div>
+          <div style={{ height: `${(IMG_HEIGTH + IMG_GAP) * DECK_SIZE + IMG_GAP}px` }} />
         </div>
         {Range(0, DECK_SIZE).map(index => (
           <div id={`${hash}_${index}`} className={ImageStyle} key={index} style={{ visibility: 'hidden' }}>
---
+++ src/components/Gallery/GelleyHelpers.ts
@@ -0,0 +1,79 @@
+import { List } from 'immutable';
+
+export type VisibilityFn = (scrollTop: number) => { start: number; end: number } | null;
+export type ElStyle = { top: string; left: string; zIndex: number; transform: string }
+export type PositionFn = (index: number, scrollTop: number) => ElStyle;
+export type RndFactor = { offsetX: number; offsetY: number; deg: number };
+
+/**
+ * Prepare function which return first and last indexes of the blocks on the screen.
+ */
+export function getVisibilityCalcFn(viewportHeight: number, visibilityPadding: number, imageHeight: number, gap: number): VisibilityFn {
+  const BLOCK = imageHeight + gap;
+
+  return (scrollTop: number): { start: number; end: number } | null => {
+    const end = scrollTop + viewportHeight;
+
+    const firstPotentialBlock = 1 + Math.floor((scrollTop - BLOCK) / BLOCK);
+    const firstBlock = scrollTop + visibilityPadding < BLOCK + firstPotentialBlock * BLOCK ? firstPotentialBlock : firstPotentialBlock + 1;
+
+    const lastPotentialBlock = Math.floor((end - gap) / BLOCK);
+    const lastBlock = end - visibilityPadding > gap + lastPotentialBlock * BLOCK ? lastPotentialBlock : lastPotentialBlock - 1;
+
+    return firstBlock > lastBlock ? null : { start: firstBlock, end: lastBlock };
+  };
+}
+
+/**
+ * Prepare function which returns styles that need to be applied to the image based on current scrollTop position.
+ */
+export function getPositionCalcFn(vh: number, padding: number, imgHeight: number, gap: number, rndList: List<RndFactor>, deckSize: number): PositionFn {
+  const BLOCK = imgHeight + gap;
+  const VISIBILITY_ZONE = vh - 2 * padding;
+
+  function calcX(top: number, initialOffset: number, initialDeg: number): { offsetX: number; deg: number; scale: number } {
+    const kCenter = Math.abs(vh / 2 - (top + imgHeight / 2)) / (VISIBILITY_ZONE / 2 + imgHeight / 2);
+    return { offsetX: quadFn(initialOffset, kCenter), deg: quadFn(initialDeg, kCenter), scale: scaleFn(kCenter) };
+  }
+
+  return (index: number, scrollTop: number): ElStyle => {
+    const rnd: RndFactor = rndList.get(index) || { offsetX: 0, offsetY: 0, deg: 0 };
+    const topLine = scrollTop + padding + rnd.offsetY;
+    const bottomLine = scrollTop + vh - padding - rnd.offsetY;
+
+    let top: number, zIndex: number;
+
+    //If Block is higher than top bar
+    if (BLOCK * (index + 1) < topLine) {
+      top = padding - imgHeight + rnd.offsetY;
+      zIndex = index;
+    } else if (BLOCK * (index + 1) - imgHeight > bottomLine) {
+      top = vh - padding - rnd.offsetY;
+      zIndex = deckSize - 1 - index;
+    } else {
+      top = BLOCK * (index + 1) - imgHeight - scrollTop;
+      zIndex = deckSize;
+    }
+
+    const { offsetX, deg, scale } = calcX(top, rnd.offsetX, rnd.deg);
+
+    return {
+      top: `${top}px`,
+      left: `calc(15vw + ${offsetX}px)`,
+      transform: `rotate(${deg}deg) scale(${scale}) translateZ(0)`,
+      zIndex
+    };
+  };
+}
+
+/**
+ * Polinomial function for scale factor calculations (-1 <= x <= 1).
+ * https://www.desmos.com/calculator/0fwuwn2ni0
+ */
+export function scaleFn(x: number): number {
+  return -0.7 * Math.pow(x, 4) + 1;
+}
+
+export function quadFn(a: number, x: number): number {
+  return a * x * x;
+}
